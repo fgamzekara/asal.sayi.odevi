3d5a80a45914abc9f89a20b7aaa1d37838b46e48
khas <khas@KHASPC1036>
2022-12-26 13:55:40 +0300

    Add project files.

---
+++ asal.sayi.odevi/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sayi = 0;
+        bool durum = true;
+        while (sayi<100)
+        {
+            sayi++;
+            for (int i =2; i<sayi; i++)
+            {
+                if (sayi % i == 0) durum = false;
+                
+            }
+            if (durum == true) Console.WriteLine(sayi);
+            durum = true;
+        }
+         Console.ReadKey();
+    }
+}
